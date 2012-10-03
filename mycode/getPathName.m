6ff8dda9e9a03c1924ab71a9c3f03b4a444fb07d
Jean-Francois Lalonde <jf.lalonde@disneyresearch.com>
2012-10-03 15:05:40 -0400

    Added path functions.

---
+++ mycode/getPathName.m
@@ -0,0 +1,57 @@
+function path = getPathName(str, varargin)
+% Returns path name for the illumination estimation project.
+%
+%   path = getPathName(str, varargin)
+%
+%   Here, 'varargin' works as in the matlab built-in 'fullfile', i.e. it
+%   concatenates other strings into paths.
+%
+% See also:
+%   fullfile
+% 
+% ----------
+% Jean-Francois Lalonde
+
+% use path to 'getPathName' to retrieve the base path
+basePath = fileparts(fileparts(fileparts(fileparts(fileparts(which('getPathName.m'))))));
+
+resultsBasePath = fullfile(basePath, 'results');
+codeBasePath = fullfile(basePath, 'code', 'projects');
+
+projectName = 'shadowDetection';
+
+if nargin == 0 || isempty(str)
+    fprintf(['Options: ''code'', ''codeUtils'', ''codeSkyModel'', ''results'''...
+        ', ''data'', ''status'', ''logs''.\n']);
+    if nargout == 1
+        path = '';
+    end
+else
+    
+    switch(str)
+        case 'code'
+            path = fullfile(codeBasePath, projectName);
+
+        case 'codeUtils'
+            path = fullfile(codeBasePath, 'utils');
+            
+        case 'results'
+            path = fullfile(resultsBasePath, projectName);
+
+        case 'data'
+            path = fullfile(basePath, 'data', projectName);
+
+        case 'status'
+            path = fullfile(resultsBasePath, projectName, 'status');
+            
+        case 'logs'
+            path = fullfile(basePath, 'logs');
+            
+        otherwise
+            error('Invalid option');
+    end
+
+    if ~isempty(varargin)
+        path = fullfile(path, varargin{:});
+    end
+end
---
+++ mycode/setPath.m
@@ -0,0 +1,31 @@
+% Path setup for the shadow detection project
+
+clear global;
+restoredefaultpath;
+% global logFileRoot;
+
+% host-dependent paths
+[a, host] = system('hostname');
+
+% logFileRoot = getPathName('logs');
+path3rdParty = getPathName('code', '3rd_party');
+pathMyCode = getPathName('code', 'mycode');
+pathUtils = getPathName('codeUtils');
+
+%% Turn off some annoying warnings
+warning off Images:initSize:adjustingMag;
+
+randSeed = sum(100*clock) + sum(host);
+if ~isempty(strfind(version, '7.7'))
+    RandStream.setDefaultStream(RandStream('mt19937ar','seed', randSeed))
+else
+    rand('twister', randSeed);
+end
+
+% Restore to initial state
+restoredefaultpath;
+
+%% Setup project paths
+addpath(genpath(pathMyCode));
+addpath(genpath(pathUtils));
+addpath(genpath(path3rdParty));
